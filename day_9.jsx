4bf44743fc4a8131a346a1990c02d1655453e004
Rahul936-git <rahulshaw8603@gmail.com>
2025-05-26 02:09:41 +0530

    Nith commit

---
+++ day_9.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactDOM from "react-dom/client";
+
+import Header from "../Component/Header";
+import Body from "../Component/Body";
+
+function GitHubProfile(){
+
+    return(
+        <>
+        <Header/>
+        <Body/>
+        </>
+    )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<GitHubProfile/>);
