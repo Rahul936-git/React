9ccef8d0b8c05eed560f00611d407f55ca2f4732
Rahul936-git <rahulshaw8603@gmail.com>
2025-06-15 07:53:48 +0530

    Tenth

---
+++ script.jsx
@@ -0,0 +1,69 @@
+import React, { useCallback, useEffect, useMemo, useState }  from "react";
+import ReactDOM from "react-dom/client";
+
+// useMemo hook
+// 1. counter button : increase
+// 2. input field : fibonnaci number
+// 0 1 1 2 3 5 8 13 21 34......
+// Recursve code time complexiy : 2^n
+
+
+//   function Fibonnaci(n){
+//         if(n<=1) return n;
+
+//         else return Fibonnaci(n-1)+Fibonnaci(n-2);
+//     }
+
+
+
+function App(){
+
+    const [count,setCount]=useState(0);
+    const [number,setnumber]=useState("");
+
+    // const[results,setResults]=useState("");       // this state variable is created only to use useEffect
+
+    // function Fibonnaci(n){
+    //     if(n<=1) return n;
+
+    //     else return Fibonnaci(n-1)+Fibonnaci(n-2);
+    // }
+
+                                                    // // use callback function
+
+    const Fibonnaci = useCallback((n)=>{
+        if(n<=1)
+            return n;
+
+        else  return Fibonnaci(n-1) + Fibonnaci(n-2);
+    },[])   
+                                                      // no dependicies []
+
+
+    // const result = Fibonnaci(number);
+
+    // useEffect(()=>{                                 // useEffect always executed in last
+    //     setResults(Fibonnaci(number));
+    // },[number]);
+
+     const result = useMemo(()=>Fibonnaci(number),[number]);  //[dependencies]  // useMemo save extra render unlike useEffect take one extra render
+
+    return(
+        <>
+        <h1>Counter is : {count}</h1>
+        <button onClick={()=>setCount(count+1)}>Increment</button>
+        <button onClick={()=>setCount(count-1)}>Decrement</button>
+
+        <div>
+            {/* <h2>Fibonnaci number is: {results}</h2> */}              {/* useEffect */}
+            <h2>Fibonnaci number is :{result}</h2>                        {/* useMemo */}
+            <input type="number" value={number} onChange={(e)=>setnumber(e.target.value)}></input>
+        </div>
+        </>
+    )
+}
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App></App>);
